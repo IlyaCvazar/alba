2ba2ec6a7fee4218ff1a96fe51566eb4fb890dc9
Ivan-Zv <sasiska117117@gmail.com>
2026-05-16 13:13:06 +0700

    пробник библиотеки

--- client.py
+++ client.py
@@ -1,13 +1,15 @@
 import flet as ft
 
+print("hello")
+
 def main(page: ft.Page):
     page.title = "Мобильное приложение на Flet"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
-    
     txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
     txt_number2 = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
     
     def minus_click(e):
+        print("hello")
         txt_number.value = str(int(txt_number.value) - 1)
         page.update()
     def minus_click2(e):
